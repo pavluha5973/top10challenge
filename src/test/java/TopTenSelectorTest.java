25cf161e9fccf21e05298e776330d6aab7c30916
pgorovik@sws.by <pavluha5973@gmail.com>
2019-09-17 12:49:52 +0300

    Javadoc for test

--- src/test/java/TopTenSelectorTest.java
+++ src/test/java/TopTenSelectorTest.java
@@ -8,8 +8,11 @@ public class TopTenSelectorTest {
 
   private TopTenSelector topTenSelector = new TopTenSelectorImpl();
 
+  /**
+   * Test of the {@link TopTenSelector#topN(Stream)} method with a stream of 100000000 integers
+   */
   @Test
-  public void test() {
+  public void testTopN() {
     int limit = 100000000;
     Stream<Integer> stream = Stream.iterate(0, i -> i + 1).limit(limit);
     List<Integer> list = topTenSelector.topN(stream);
