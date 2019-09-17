97a70007867684dfa6abbcc95505feb0a95e3aff
pgorovik@sws.by <pavluha5973@gmail.com>
2019-09-17 12:38:31 +0300

    add Javadoc

--- src/main/java/by/sws/challenge/queue/BoundedPriorityQueue.java
+++ src/main/java/by/sws/challenge/queue/BoundedPriorityQueue.java
@@ -2,12 +2,18 @@ package by.sws.challenge.queue;
 
 import java.util.PriorityQueue;
 
+/**
+ * This is extension of {@code PriorityQueue} which has number of integers not greater than bound
+ * specified by user in {@link #BoundedPriorityQueue(int)}. The {@link
+ * #BoundedPriorityQueue#offer(Integer)} is overriden: it adds integer to the priority queue and
+ * then if the size of it is greater than bound then method polls the least integer.
+ */
 public class BoundedPriorityQueue extends PriorityQueue<Integer> {
 
   private int bound;
 
   public BoundedPriorityQueue(int bound) {
-    super(bound + 1);
+    super(bound + 1); // the maximum possible size of this queue is bound + 1
     this.bound = bound;
   }
 
